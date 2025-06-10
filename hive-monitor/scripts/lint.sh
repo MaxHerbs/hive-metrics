0b52cf3400f5744fcca8d0699d17f1a0a82a0d8f
Max Herbert <max@maxherbert.com>
2025-06-10 22:20:41 +0100

    fix: restructured cpp linter

--- hive-monitor/scripts/lint.sh
+++ hive-monitor/scripts/lint.sh
@@ -1,2 +1,4 @@
 cpplint --repository=. --recursive --linelength=120 --verbose=4 --filter=-build/include,-build/header_guard ./src
-cpplint --repository=. --recursive --exclude ./lib/ArduinoJson --linelength=120 --verbose=4 --filter=-build/include,-build/header_guard ./lib
+cpplint --repository=. --recursive --linelength=120 --verbose=4 --filter=-build/include,-build/header_guard ./lib/ConfigManager
+cpplint --repository=. --recursive --linelength=120 --verbose=4 --filter=-build/include,-build/header_guard ./lib/Settings
+cpplint --repository=. --recursive --linelength=120 --verbose=4 --filter=-build/include,-build/header_guard ./lib/TempSensor
