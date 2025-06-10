d4d92705d6e132d3ce2967f4251f892dcbf6590a
Max Herbert <max@maxherbert.com>
2025-06-10 23:27:42 +0100

    fix(proxy) better logging

--- otel-proxy/src/routes/mod.rs
+++ otel-proxy/src/routes/mod.rs
@@ -1,21 +1,22 @@
+pub mod health_check;
 use crate::AppState;
 use std::{collections::HashMap, sync::Arc};
 
 use axum::{extract::State, http::StatusCode, Json};
 use opentelemetry::KeyValue;
 use serde::{Deserialize, Serialize};
+use tracing::{debug, error};
 
 pub async fn log_metric(
     State(state): State<Arc<AppState>>,
-    // headers: HeaderMap,
     Json(body): Json<MetricBody>,
 ) -> (StatusCode, Json<MetricsResponse>) {
-    println!("{:#?}", body);
+    debug!("Incoming log with body:\n{:#?}", body);
 
     let mut unsupported_metrics: Vec<String> = Vec::new();
     body.metrics.into_iter().for_each(|(metric_name, val)| {
         if let Some(metric_fn) = state.metric_map.get(&metric_name) {
-            println!("Found metric for {}", metric_name);
+            debug!("Found metric for {}", metric_name);
             metric_fn.record(
                 val,
                 &[
@@ -24,13 +25,14 @@ pub async fn log_metric(
                 ],
             );
         } else {
-            println!("Unsupported metric called: {}", &metric_name);
+            error!("Unsupported metric called: {}", &metric_name);
             unsupported_metrics.push(metric_name);
         }
     });
 
     if !unsupported_metrics.is_empty() {
         let error = format!("Unsupported metrics:{}", unsupported_metrics.join("\n"));
+        error!("{}", error);
         return (
             StatusCode::BAD_REQUEST,
             Json(MetricsResponse {
